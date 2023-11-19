5863e55d6f72298e9e69585cc80af92e01df40a4
Boierul <49337867+Boierul@users.noreply.github.com>
2023-11-19 14:36:25 +0100

    Fix: fixed the bug of the jumpscare animation
    Co-Authored-By: Vladimir Rotaru <31960682+senker@users.noreply.github.com>
    Co-Authored-By: Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>

--- broom/Assets/Scripts/JumpscareTrigger.cs
+++ broom/Assets/Scripts/JumpscareTrigger.cs
@@ -8,18 +8,38 @@ public class JumpscareTrigger : MonoBehaviour
 
     public AudioSource jumpscareAudio;
 
+    // If you're using 3D physics, you might need to use Physics.
+    // Raycast to detect the mouse position. 
+    // The OnMouseOver method is primarily used in 2D scenarios. 
+    // If you're working in a 3D environment, you may need to use raycasting instead.
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == gameObject)
+        {
+            OnMouseOver();
+        }
+    }
+
     // This is called when the player enters the trigger
-    public void OnMouseOver() {
+    public void OnMouseOver()
+    {
+        Debug.Log("Mouse is over GameObject.");
 
         // Play the animation
         jumpscareAnimation.Play();
 
         // If jumpscareAudio exists, play it
-        if (jumpscareAudio != null) {
+        if (jumpscareAudio != null)
+        {
             jumpscareAudio.Play();
         }
 
         // Disable the trigger so it can't be triggered again (no infinite loop)
         this.gameObject.SetActive(false);
+
+        Debug.Log("MouseOver finished");
     }
 }
