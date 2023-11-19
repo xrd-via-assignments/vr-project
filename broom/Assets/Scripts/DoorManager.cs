c6c6c30cd7cd04784f818f5e7f6abe736780c7cb
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2023-11-19 16:23:30 +0100

    fix: tracking the opened door with a boolean

--- broom/Assets/Scripts/DoorManager.cs
+++ broom/Assets/Scripts/DoorManager.cs
@@ -5,15 +5,21 @@ public class DoorManager : MonoBehaviour
     public Animation Door;
     public AudioSource DoorOpenSound;
 
+    private bool hasDoorBeenOpened = false;
+
     void Update()
     {
         if (PlayerCasting.DistanceFromTarget < 4)
         {
             if (Input.GetKey(KeyCode.JoystickButton0))
             {
-                GetComponent<BoxCollider>().enabled = false;
-                Door.Play();
-                DoorOpenSound.Play();
+                if (!hasDoorBeenOpened && !Door.isPlaying)
+                {
+                    GetComponent<BoxCollider>().enabled = false;
+                    Door.Play();
+                    DoorOpenSound.Play();
+                    hasDoorBeenOpened = true; // Set the flag to indicate that the door has been opened
+                }
             }
         }
     }
