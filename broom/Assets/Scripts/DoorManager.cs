beadbc8dbade4559628ef142dbe4bffeb52c1491
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2023-11-19 15:46:19 +0100

    fix: removing unnecessary code
    Co-Authored-By: Dan Pintea <49337867+Boierul@users.noreply.github.com>
    Co-Authored-By: Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>

--- broom/Assets/Scripts/DoorManager.cs
+++ broom/Assets/Scripts/DoorManager.cs
@@ -1,40 +1,9 @@
-using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class DoorManager : MonoBehaviour
 {
     public Animation Door;
     public AudioSource DoorOpenSound;
-
-    private InputAction buttonAction;
-    private bool isButtonPressed = false;
-
-    void Start()
-    {
-        // Set up the input action for the A button
-        buttonAction = new InputAction(binding: "<XRController>/buttonSouth", type: InputActionType.Button);
-        buttonAction.started += context => isButtonPressed = true;
-        buttonAction.canceled += context => isButtonPressed = false;
-
-        // Ensure that buttonAction is not null before enabling
-        if (buttonAction != null)
-            buttonAction.Enable();
-    }
-
-    void OnEnable()
-    {
-        // Ensure that buttonAction is not null before enabling
-        if (buttonAction != null)
-            buttonAction.Enable();
-    }
-
-    void OnDisable()
-    {
-        // Ensure that buttonAction is not null before disabling
-        if (buttonAction != null)
-            buttonAction.Disable();
-    }
 
     void Update()
     {
@@ -42,7 +11,6 @@ public class DoorManager : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.JoystickButton0))
             {
-                Debug.Log("yaybal");
                 GetComponent<BoxCollider>().enabled = false;
                 Door.Play();
                 DoorOpenSound.Play();
