991589fa845273f94b6e4b46925208a798562045
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2023-11-19 15:26:43 +0100

    feat: door opened by jostick command
    Co-Authored-By: Dan Pintea <49337867+Boierul@users.noreply.github.com>
    Co-Authored-By: Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>

--- broom/Assets/Scripts/DoorManager.cs
+++ broom/Assets/Scripts/DoorManager.cs
@@ -1,62 +1,52 @@
-// Coded by Developer Jake -- https://www.youtube.com/developerjake
-// Follow the Backrooms Game Lab (Part 6) to understand what this is for
+using System;
 using UnityEngine;
-using UnityEngine.XR;
+using UnityEngine.InputSystem;
 
-public class DoorManager : MonoBehaviour // This script should be on the Door Trigger
+public class DoorManager : MonoBehaviour
 {
-    public GameObject CursorHover; // The hover cursor that should show when the player is looking at the door
-
     public Animation Door;
-
     public AudioSource DoorOpenSound;
 
-    private Vector2 inputAxisLeft;
-    private Vector2 inputAxisRight;
+    private InputAction buttonAction;
+    private bool isButtonPressed = false;
 
-    public XRNode inputSourceLeft;
-    public XRNode inputSourceRight;
-
-    private void OnMouseOver() // Activates when the player looks away from the door
+    void Start()
     {
+        // Set up the input action for the A button
+        buttonAction = new InputAction(binding: "<XRController>/buttonSouth", type: InputActionType.Button);
+        buttonAction.started += context => isButtonPressed = true;
+        buttonAction.canceled += context => isButtonPressed = false;
 
-        if (PlayerCasting.DistanceFromTarget < 4) // If the player IS close enough to the door..
-        {
-
-            //CursorHover.SetActive(true);
-
-        InputDevice deviceLeft = InputDevices.GetDeviceAtXRNode(inputSourceLeft);
-        InputDevice deviceRight = InputDevices.GetDeviceAtXRNode(inputSourceRight);
-        deviceLeft.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxisLeft);
-        deviceRight.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxisRight);
-
-            if (deviceRight.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > 0.5f) // If the player presses E..
-            {
-
-                GetComponent<BoxCollider>().enabled = false; // Turns off the player's ability to open the door again even though it's already open
-
-                Door.Play(); // Play the door open animation
-
-                DoorOpenSound.Play(); // Play the door open sound
-
-            }
-
-        }
-
-        /*         else // If the player is NOT close enough to the door
-                {
-
-                    CursorHover.SetActive(false);
-
-                } */
+        // Ensure that buttonAction is not null before enabling
+        if (buttonAction != null)
+            buttonAction.Enable();
     }
 
-
-
-    private void OnMouseExit() // Activates when the player looks away from the door
+    void OnEnable()
     {
+        // Ensure that buttonAction is not null before enabling
+        if (buttonAction != null)
+            buttonAction.Enable();
+    }
 
-        CursorHover.SetActive(false);
+    void OnDisable()
+    {
+        // Ensure that buttonAction is not null before disabling
+        if (buttonAction != null)
+            buttonAction.Disable();
+    }
 
+    void Update()
+    {
+        if (PlayerCasting.DistanceFromTarget < 4)
+        {
+            if (Input.GetKey(KeyCode.JoystickButton0))
+            {
+                Debug.Log("yaybal");
+                GetComponent<BoxCollider>().enabled = false;
+                Door.Play();
+                DoorOpenSound.Play();
+            }
+        }
     }
 }
