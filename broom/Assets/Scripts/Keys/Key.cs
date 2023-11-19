055b341d024ccef56f6df96a4e63b418ba281120
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2023-11-19 17:28:52 +0100

    fix: key.cs code readjustments
    Co-Authored-By: Dan Pintea <49337867+Boierul@users.noreply.github.com>
    Co-Authored-By: Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>

--- broom/Assets/Scripts/Keys/Key.cs
+++ broom/Assets/Scripts/Keys/Key.cs
@@ -9,34 +9,23 @@ public class Key : MonoBehaviour
 {
     [Tooltip("The name of the key. This corresponds with the key on the door")] public string keyName;
 
-    public GameObject HoverIcon;
-
     [Header("(Optional)")]
     [Tooltip("(Optional.)")] public AudioClip CollectAudio;
 
-    public void OnMouseOver()
+    public void Update()
     {
         if (PlayerCasting.DistanceFromTarget <= 5) // Is the player in range?
         {
-            HoverIcon.SetActive(true);
 
-            if (Input.GetKeyDown(KeyCode.E))
+            if (Input.GetKey(KeyCode.JoystickButton0))
             {  // These events happen when the player is looking at the key, and is in range, and presses E.
 
                 if (CollectAudio != null) FindObjectOfType<KeyManager>().GetComponent<AudioSource>().PlayOneShot(CollectAudio); // (Optional.)
 
                 FindObjectOfType<KeyManager>().keysInInventory.Add(keyName);
 
-                HoverIcon.SetActive(false);
-
                 Destroy(this.gameObject);
             }
         }
-        else HoverIcon.SetActive(false);
-    }
-
-    public void OnMouseExit()
-    {
-        HoverIcon.SetActive(false);
     }
 }
